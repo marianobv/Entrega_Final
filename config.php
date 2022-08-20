7d68eb3f92deac47addc0a49660d34cf3b0f8284
marianobv <ingenieromarianoblancovieira@gmail.com>
2022-08-20 13:21:03 -0300

    20_08

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 //init configuration
 $clientID = 800497960446-n73rnistajq6h5o9m4g0n42c6pihavbu.apps.googleusercontent.com;
-$clientSecret = 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;
-$redirectUri = 'https://marianobv.github.io/Parte-2/';
+$clientSecret = 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;
+$redirectUri = 'https://marianobv.github.io/Parte-2/index_2.html';
